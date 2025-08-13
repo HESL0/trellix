acc2a99b74722f6c7567893daee74fc4bd3bf0c2
AMIR <evil2000devil@gmail.com>
2025-08-13 15:49:49 +0330

    `Refactor CardsPage to include AppHeader and improve card adding functionality`

---
+++ src/components/planner/AppPlanner.vue
@@ -0,0 +1,28 @@
+<template>
+  <editor-content :editor="editor" />
+</template>
+
+<script setup>
+import { Editor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+import { onBeforeUnmount, ref } from 'vue'
+
+const editor = ref(null)
+
+editor.value = new Editor({
+  content: '<p>Start typing here...</p>',
+  extensions: [StarterKit],
+})
+onBeforeUnmount(() => {
+  editor.value.destroy()
+})
+</script>
+
+<style>
+.ProseMirror {
+  min-height: 150px;
+  border: 1px solid #ccc;
+  padding: 10px;
+  outline: none;
+}
+</style>
--- src/pages/CardsPage.vue
+++ src/pages/CardsPage.vue
@@ -1,42 +1,97 @@
 <template>
-  <div class="row q-col-gutter-md">
-    <div v-for="card in cards" :key="card.id" class="col-12 col-sm-6 col-md-4">
+  <AppHeader/>
+
+  <div class="flex no-wrap q-gutter-md overflow-auto q-pa-md">
+    <div
+      v-for="card in cards"
+      :key="card.id"
+      class="flex-none"
+      style="min-width: 300px;"
+    >
       <AppCard :card="card" />
     </div>
 
-    <div class="col-12 col-sm-6 col-md-4">
-      <q-card class="q-pa-md flex flex-center cursor-pointer" @click="showPrompt = true">
-        <q-icon name="add" size="md" />
+    <div class="flex-none" style="min-width: 300px;">
+      <q-card class="q-pa-md">
+        <div v-if="!isAddingCard" class="flex flex-center cursor-pointer" @click="startAddingCard">
+          <q-icon name="add" size="md" />
+        </div>
+        
+        <div v-else class="q-gutter-sm">
+          <q-input
+            v-model="newTitle"
+            placeholder="Enter card title"
+            dense
+            autofocus
+            @keyup.enter="addCard"
+            @keyup.esc="cancelAddingCard"
+            ref="titleInput"
+          />
+          <div class="row q-gutter-xs">
+            <q-btn
+              label="Add"
+              color="primary"
+              size="sm"
+              @click="addCard"
+              :disable="!newTitle.trim()"
+            />
+            <q-btn
+              label="Cancel"
+              size="sm"
+              flat
+              @click="cancelAddingCard"
+            />
+          </div>
+        </div>
       </q-card>
     </div>
-
-    <q-dialog v-model="showPrompt" persistent>
-      <q-card>
-        <q-card-section>
-          <q-input v-model="newTitle" label="Card Title" autofocus />
-        </q-card-section>
-        <q-card-actions align="right">
-          <q-btn flat label="Cancel" v-close-popup />
-          <q-btn flat label="Add" @click="addCard" />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
   </div>
 </template>
+
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, nextTick } from 'vue'
 import { useCardStore } from '../stores/cardStore'
 import AppCard from '../components/card/AppCard.vue'
+import AppHeader from 'src/components/layout/AppHeader.vue'
 
 const cardStore = useCardStore()
-const showPrompt = ref(false)
+const isAddingCard = ref(false)
 const newTitle = ref('')
+const titleInput = ref(null)
 
 const cards = computed(() => cardStore.cards)
 
-function addCard() {
-  cardStore.addCard(newTitle.value)
+function startAddingCard() {
+  isAddingCard.value = true
   newTitle.value = ''
-  showPrompt.value = false
+  nextTick(() => {
+    titleInput.value?.focus()
+  })
+}
+
+function addCard() {
+  if (newTitle.value.trim()) {
+    cardStore.addCard(newTitle.value.trim())
+    newTitle.value = ''
+    isAddingCard.value = false
+  }
+}
+
+function cancelAddingCard() {
+  newTitle.value = ''
+  isAddingCard.value = false
 }
 </script>
+
+<style scoped>
+.overflow-auto {
+  overflow-x: auto;
+  overflow-y: hidden;
+  padding-bottom: 8px;
+  scrollbar-width: none; /* Firefox */
+  -ms-overflow-style: none; /* IE/Edge */
+}
+.overflow-auto::-webkit-scrollbar {
+  display: none; /* Chrome/Safari/Opera */
+}
+</style>
